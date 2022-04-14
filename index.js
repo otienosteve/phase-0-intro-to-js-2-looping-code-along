2ff4c4d51301aab1fe8818e2cb6c182a9db63866
otienosteve <steveotieno701@gmail.com>
2022-04-14 21:48:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr){
+    var arr2=[]
+for(var i=0; i<=arr.length-1; i++){
+    
+    arr2.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    
+}
+    return arr2;
+}
+function countDown(num){
+for(var f=num; f>=0; f--){
+    console.log(f)
+}
+}
